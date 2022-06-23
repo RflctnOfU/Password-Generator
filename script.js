c7fd8b660d933bd43eb7d7fbb8bc77d0cf57b702
Kris Marshall <rflctnofu@hotmail.com>
2022-06-23 18:40:27 -0400

    started creating var's for the generator script

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+characterSet = {
+  lowerCaseCharacters: "abcdefghijklmnopqrstuvwxyz",
+  upperCaseCharacters: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numberCharacters: "0123456789",
+  specialCharacters: "`~!@#$%^&*()"
+}
+
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
+  var pwLength = prompt("How long would you like your password? 8-128");
+    // console.log(pwLength);
+  alert("You chose " + pwLength);
+  
+  var lowerCase = prompt("Would you like to use lower case letters? y/n");
+
+  alert("You chose " + lowerCase);
+
+  var upperCase = prompt("Would you like to use upper case letters? y/n");
+
+  alert("You chose " + upperCase);
+
+  var numberCase = prompt("Would you like to use numbers? y/n");
+
+  alert("You chose " + numberCase);
+
+  var specialCase = prompt("Would you like to use special characters? y/n");
+
+  alert("You chose " + numberCase);
 
   passwordText.value = password;
 
