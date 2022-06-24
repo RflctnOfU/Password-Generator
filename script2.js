05e35808230a519e68e12542bcf45436092382eb
Kris Marshall <rflctnofu@hotmail.com>
2022-06-24 18:51:36 -0400

    finished character selection

--- script2.js
+++ script2.js
@@ -2,7 +2,12 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-    var passwordChar;
+    var lowerCase;
+    var upperCase;
+    var numberCase;
+    var symbolCase;
+    var passwordLength;
+    var passwordChar = "";
     var lowerCaseChar = "abcdefghijklmnopqrstuvwxyz";
     var upperCaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var numberCaseChar = "0123456789";
@@ -10,32 +15,48 @@ function generatePassword() {
     // 1. prompt user
         // pw length - error if outside range to validate
     function promptLength() {
-        var passwordLength = parseInt(prompt("Select a password length 8-128."));
+        passwordLength = parseInt(prompt("Select a password length 8-128."));
         while ((passwordLength < 8) || (passwordLength > 128)) {
         alert("Error, value out of range.");
         return promptLength();
         } 
+
     }
 
     // confirm character types - lower, upper, numeric, symbols, and validate that at least one was selected
 
     function characterTypes() {
-        var lowerCase = confirm("Would you like to include lower case letters?");
+        lowerCase = confirm("Would you like to include lower case letters?");
 
-        var upperCase = confirm("Would you like to include upper case letters?");
+        upperCase = confirm("Would you like to include upper case letters?");
 
-        var numberCase = confirm("Would you like to include numbers?");
+        numberCase = confirm("Would you like to include numbers?");
 
-        var symbolCase = confirm("Would you like to include symbolss?");
+        symbolCase = confirm("Would you like to include symbolss?");
 
         while ((lowerCase === false) && (upperCase === false) && (numberCase === false) && (symbolCase === false)) {
             alert("You must select at least one character type.")
             return characterTypes();
         }
 
+        // adding selections of character types to possibile choices for random selection
+
         if (lowerCase === true) {
-            passwordChar += lowerCase;
+            passwordChar += lowerCaseChar;
         }
+
+        if (upperCase === true) {
+            passwordChar += upperCaseChar;
+        }
+
+        if (numberCase === true) {
+            passwordChar += numberCaseChar;
+        }
+
+        if (symbolCase === true) {
+            passwordChar += symbolCaseChar;
+        }
+        
     }
 
 
@@ -49,14 +70,7 @@ function generatePassword() {
   
     
 
-    
-    // var lowerCase = confirm("Would you like to include lower case letters?");
 
-    // var upperCase = confirm("Would you like to include upper case letters?");
-
-    // var numberCase = confirm("Would you like to include numbers?");
-
-    // var symbolCase = confirm("Would you like to include symbolss?");
         
     
     // 4. pw is then written to page    
